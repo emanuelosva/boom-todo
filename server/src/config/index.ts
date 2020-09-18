4dac4bc7670c50c6f650df28309411bdc2969541
emanuelosva <emanuelosva@gmail.com>
2020-09-17 22:46:46 -0500

    Add config file

---
+++ server/src/config/index.ts
@@ -0,0 +1,19 @@
+/**
+ * **************************
+ * @fileoverview App env vars
+ * **************************
+ */
+
+import * as dotenv from 'dotenv'
+
+dotenv.config({ path: '../.env' })
+
+export const config = {
+  app: {
+    port: process.env.PORT || 3000,
+    dev: process.env.NODE_ENV !== 'production',
+  },
+  auth: {
+    secret: process.env.JWT_SECRET,
+  }
+}
