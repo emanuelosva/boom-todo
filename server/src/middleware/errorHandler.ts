a385673bc218f5ca6e633060f612b2d770c2c06c
emanuelosva <emanuelosva@gmail.com>
2020-09-20 17:55:12 -0500

    Add logging to error middleware

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -5,16 +5,30 @@
  */
 
 import express from 'express'
+import debug from 'debug'
 import { ErrorResponse } from '../lib/errorClass'
 import { responseError } from '../network/response'
+
+const errorLogger = debug('app:Error')
 
 export const errorHandler = (
   err: ErrorResponse,
   req: express.Request,
   res: express.Response,
+  next: express.NextFunction,
 ): void => {
   const status = err.statusCode || 500
   const detail = err.message
+
+  // Log only on development
+  errorLogger('Error', err.message)
+  errorLogger('Error', err.stack)
+
+  // Send email on logic error
+  if (!err.clientError) {
+    // Todo
+    errorLogger('Send email')
+  }
 
   responseError(res, status, detail)
 }
