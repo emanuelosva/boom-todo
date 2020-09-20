c64016a1612d111e6056b0de612281a2306dbe71
emanuelosva <emanuelosva@gmail.com>
2020-09-20 17:50:48 -0500

    Add indentifier for client errors

--- server/src/lib/errorClass.ts
+++ server/src/lib/errorClass.ts
@@ -7,10 +7,12 @@
 export class ErrorResponse extends Error {
   public statusCode: number
   public message: string
+  public clientError: boolean
 
   constructor(status: number, message: string) {
     super()
     this.statusCode = status
     this.message = message
+    this.clientError = true
   }
 }
