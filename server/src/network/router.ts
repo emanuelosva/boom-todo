8a01dc5098d5bddf40108d8027846491817c5379
emanuelosva <emanuelosva@gmail.com>
2020-09-21 13:31:52 -0500

    Register todos router

--- server/src/network/router.ts
+++ server/src/network/router.ts
@@ -6,6 +6,7 @@
 
 import { Router } from 'express'
 import { router as usersRouter } from '../components/users/router'
+import { router as todosRouter } from '../components/todos/router'
 
 /**
  * General Router instance
@@ -13,3 +14,4 @@ import { router as usersRouter } from '../components/users/router'
 export const apiRouter = Router()
 
 apiRouter.use('/users', usersRouter)
+apiRouter.use('/todos', todosRouter)
