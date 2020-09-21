e9aafed3dc86ae200f82cd754251b197fe09b248
emanuelosva <emanuelosva@gmail.com>
2020-09-20 22:06:42 -0500

    Add update & delete routes

--- server/src/components/users/router.ts
+++ server/src/components/users/router.ts
@@ -10,7 +10,12 @@ import { responseSuccess } from '../../network/response'
 import { ErrorResponse } from '../../lib/errorClass'
 import { authenticateBearer } from '../../lib/auth/strategys/bearer.strategy'
 import { UserController } from './controller'
-import { userCreateSchema, userLoginSchema } from './schemas'
+import {
+  userCreateSchema,
+  userUpdateSchema,
+  userLoginSchema,
+  idSchema
+} from './schemas'
 
 /**
  * Router
@@ -58,37 +63,37 @@ router.get(
 )
 
 
-// router.patch(
-//   '/',
-//   async (
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction
-//   ) => {
-//     try {
-//       const { id } = req.params
-//       const { name, email, password } = req.body
-//       const user = await userController.update({ id, name, email, password })
-//       responseSuccess(res, user, 200, 'User updated')
-//     } catch (error) {
-//       next(error)
-//     }
-//   }
-// )
+router.put(
+  '/:id',
+  authenticateBearer(),
+  validationHandler(idSchema, { check: 'params' }),
+  validationHandler(userUpdateSchema, { check: 'body' }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const { name, email, password } = req.body
+      const user = await userController.update(
+        { id: Number(id) },
+        { name, email, password }
+      )
+      responseSuccess(res, user, 200, 'User updated')
+    } catch (error) {
+      next(error)
+    }
+  }
+)
 
-// router.delete(
-//   '/:id',
-//   async (
-//     req: express.Request,
-//     res: express.Response,
-//     next: express.NextFunction
-//   ) => {
-//     try {
-//       const { id } = req.params
-//       const user = await userController.delete({ id })
-//       responseSuccess(res, user, 200, 'User deleted')
-//     } catch (error) {
-//       next(error)
-//     }
-//   }
-// )
+router.delete(
+  '/:id',
+  authenticateBearer(),
+  validationHandler(idSchema, { check: 'params' }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { id } = req.params
+      const user = await userController.delete({ id: Number(id) })
+      responseSuccess(res, user, 200, 'User deleted')
+    } catch (error) {
+      next(error)
+    }
+  }
+)
