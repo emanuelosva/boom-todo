e0d5dbcfb033e2a7a81de6b3ff09d96333f095ad
emanuelosva <emanuelosva@gmail.com>
2020-09-21 16:24:48 -0500

    Fix auth stratgy

--- server/src/lib/auth/strategys/bearer.strategy.ts
+++ server/src/lib/auth/strategys/bearer.strategy.ts
@@ -28,26 +28,26 @@ export const authenticateBearer = () => {
   ): Promise<void> => {
     const { authorization } = req.headers
     if (!authorization) {
-      next(new ErrorResponse(401, ''))
+      return next(new ErrorResponse(401, ''))
     }
 
     const token = authorization?.split(' ')[1] || ''
     if (!token) {
-      next(new ErrorResponse(401, ''))
+      return next(new ErrorResponse(401, ''))
     }
 
     try {
       const { userId } = verifyToken(token)
       const user = await prisma.user.findOne({ where: { id: userId } })
       if (!user) {
-        next(new ErrorResponse(401, ''))
+        return next(new ErrorResponse(401, ''))
 
       }
       req.user = user
-      next()
+      return next()
     } catch (error) {
       req.user = null
-      next(new ErrorResponse(401, ''))
+      return next(new ErrorResponse(401, ''))
     }
   }
 }
