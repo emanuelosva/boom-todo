9180ef83d48b38d7c094d71ad34554bcbaeec58e
emanuelosva <emanuelosva@gmail.com>
2020-09-21 21:29:01 -0500

    Implement auth routes

--- frontend/src/routes/api/login.json.js
+++ frontend/src/routes/api/login.json.js
@@ -4,8 +4,37 @@
  * *************************
  */
 
+import axios from 'axios';
+import { config } from '../../config';
+
 export async function post(req, res) {
-  //todo...
-  res.status(200).json({})
-  res.end()
-}
+  try {
+    const { email, password } = req.body;
+
+    // Make a login to the API
+    const { data } = await axios({
+      method: 'POST',
+      url: `${config.api.url}/users/login`,
+      data: { email, password }
+    });
+
+    const { user, token } = data.data;
+
+    // Set token and user Id on cookies to manage session
+    res.cookie('token', token, {
+      maxAge: config.app.sessionTime, // 2 Days
+      httpOnly: !config.app.dev,
+      secure: !config.app.dev,
+    });
+    res.cookie('user', user.id, {
+      maxAge: config.app.sessionTime, // 2 Days
+      httpOnly: !config.app.dev,
+      secure: !config.app.dev,
+    });
+
+    res.status(200).json({ user, token }).end();
+  } catch (error) {
+    const { status, data } = error.response;
+    return res.status(status).json(data).end();
+  }
+};
--- frontend/src/routes/api/signup.json.js
+++ frontend/src/routes/api/signup.json.js
@@ -4,8 +4,36 @@
  * **************************
  */
 
+import axios from 'axios';
+import { config } from '../../config';
+
 export async function post(req, res) {
-  //todo...
-  res.status(201).json({})
-  res.end()
-}
+  try {
+    const { name, email, password } = req.body;
+
+    // Make a signup to the API
+    const { data } = await axios({
+      method: 'POST',
+      url: `${config.api.url}/users/signup`,
+      data: { name, email, password }
+    });
+    const { user, token } = data.data;
+
+    res.cookie('token', token, {
+      maxAge: config.app.sessionTime, // 2 Days
+      httpOnly: !config.app.dev,
+      secure: !config.app.dev,
+    });
+    res.cookie('user', user.id, {
+      maxAge: 7200 * 1000,
+      httpOnly: !config.app.dev,
+      secure: !config.app.dev,
+    });
+
+    res.status(201).json({ user, token }).end();
+  } catch (error) {
+    const { status, data } = error.response;
+    return res.status(status).json(data).end();
+  }
+};
+
