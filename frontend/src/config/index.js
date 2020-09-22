2e99ed47a254ef3c889737befe86d8183f1fc408
emanuelosva <emanuelosva@gmail.com>
2020-09-21 21:28:50 -0500

    Add session time

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -8,8 +8,9 @@ export const config = {
   app: {
     port: process.env.PORT,
     dev: process.env.NODE_ENV !== 'production',
+    sessionTime: 2 * 24 * 3600 * 1000, // 2 Days
   },
   api: {
-    url: 'http:localhost:5000/v1',
+    url: 'http://localhost:5000/v1',
   }
 };
