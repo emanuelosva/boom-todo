ba4d8139e13d1565d5bf218ff6f4ea0dea449041
emanuelosva <emanuelosva@gmail.com>
2020-09-22 20:40:49 -0500

    Add button and warning

---
+++ frontend/src/components/SubmitButton.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let text;
+</script>
+
+<style>
+  .SubmitButton {
+    width: 80%;
+    height: 40px;
+    color: #fff;
+    font-family: "Montserrat Subrayada", sans-serif;
+    font-size: 16px;
+    border-radius: 20px;
+    border: none;
+    background-color: #ff7f51;
+    outline: none;
+  }
+</style>
+
+<button class="SubmitButton" type="submit"> {text} </button>
---
+++ frontend/src/components/WarningAuthMessage.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let message;
+</script>
+
+<style>
+  .WraningAuthMessage {
+    display: flex;
+    align-self: flex-start;
+    margin: 0 50px;
+    text-align: left;
+    color: #ebbe55;
+    font-weight: 700;
+  }
+</style>
+
+<span class="WraningAuthMessage">{message}</span>
