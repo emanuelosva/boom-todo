6d72c36393054d9d01ad3e84383bcf96a78c8873
emanuelosva <emanuelosva@gmail.com>
2020-09-22 20:41:03 -0500

    Add user store

---
+++ frontend/src/context/index.js
@@ -0,0 +1,9 @@
+/**
+ * ***************************
+ * @fileoverview Svelte stores
+ * ***************************
+ */
+
+import { writable } from 'svelte/store'
+
+export const user = writable({})
