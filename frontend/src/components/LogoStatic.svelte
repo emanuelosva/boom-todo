b550411c16d1e9ae16834f2f28bbd68f72016966
emanuelosva <emanuelosva@gmail.com>
2020-09-23 15:16:13 -0500

    Add statuc logo

---
+++ frontend/src/components/LogoStatic.svelte
@@ -0,0 +1,30 @@
+<script>
+</script>
+
+<style>
+  .Logo {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h2 {
+    margin: 0.2em 0 0 0;
+    font-family: "Montserrat Subrayada", sans-serif;
+    font-size: 1em;
+    font-weight: 700;
+    color: #fff;
+    text-shadow: 2px 2px #ff7f51;
+    /* text-shadow: 2px 2px rgba(0, 0, 0, 0.746); */
+  }
+  @media (min-width: 480px) {
+    h2 {
+      font-size: 1.8em;
+    }
+  }
+</style>
+
+<div class="Logo">
+  <h2>BoomTodo</h2>
+</div>
