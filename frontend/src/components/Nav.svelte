7a6bbe89e8ee63653f01a1ad1c8d530715424583
emanuelosva <emanuelosva@gmail.com>
2020-09-23 15:13:59 -0500

    Fix details

--- frontend/src/components/Nav.svelte
+++ frontend/src/components/Nav.svelte
@@ -12,12 +12,14 @@
   nav {
     max-width: 680px;
     margin: 0;
-    padding: 0 1em;
+    padding: 0;
     display: grid;
     grid-template-columns: minmax(auto, 680px);
     justify-content: center;
     align-items: center;
     border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
     font-weight: 400;
     background-color: #fff;
   }
@@ -46,7 +48,7 @@
   [aria-current]::after {
     position: absolute;
     content: "";
-    width: calc(100% - 1em);
+    width: calc(100% - 1.5em);
     height: 4px;
     background-color: #ff7f51;
     display: block;
