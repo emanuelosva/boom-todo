87d70fe0836885ce7363b75faab6923f2b0f5637
emanuelosva <emanuelosva@gmail.com>
2020-09-22 20:51:01 -0500

    Create a class to fetch

--- frontend/src/utils/fetchApi.js
+++ frontend/src/utils/fetchApi.js
@@ -7,7 +7,14 @@
 import axios from 'axios';
 import { config } from '../config';
 
-export const fetchApi = async (path, method, body, token) => {
+/**
+ * 
+ * @param {string} path - The endpoint to fetch
+ * @param {string} method - The http method
+ * @param {object} body - The request body
+ * @param {string} token - The JWT if the route is protected
+ */
+export const fetchApiFunc = async (path, method, body, token) => {
   try {
     const { data, status } = await axios({
       url: `${config.api.url}${path}`,
@@ -21,3 +28,26 @@ export const fetchApi = async (path, method, body, token) => {
     return { data, status }
   }
 };
+
+class FetchApi {
+  constructor() {
+    this.fetch = fetchApiFunc;
+  }
+  async get(path, token = undefined) {
+    return this.fetch(path, 'GET', {}, token)
+  }
+  async post(path, body, token = undefined) {
+    return this.fetch(path, 'POST', body, token)
+  }
+  async put(path, body, token = undefined) {
+    return this.fetch(path, 'PUT', body, token)
+  }
+  async patch(path, body, token = undefined) {
+    return this.fetch(path, 'PATCH', body, token)
+  }
+  async delete(path, body, token = undefined) {
+    return this.fetch(path, 'DELETE', body, token)
+  }
+}
+
+export const fetchApi = new FetchApi()
