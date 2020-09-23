0095a4691f93cb72ccd77f64c6ed0422b09018fa
emanuelosva <emanuelosva@gmail.com>
2020-09-23 15:15:40 -0500

    Add layout structure

--- frontend/src/routes/workspace/_layout.svelte
+++ frontend/src/routes/workspace/_layout.svelte
@@ -9,6 +9,7 @@
   export let sessionActive;
   export let segment;
   import Nav from "../../components/Nav.svelte";
+  import LogoStatic from "../../components/LogoStatic.svelte";
 </script>
 
 <style>
@@ -17,17 +18,24 @@
     padding: 0;
   }
   .TodoBoom-content {
-    margin: 0;
+    margin: 10px 0 0 0;
     padding: 0;
+    display: grid;
+    grid-template-columns: minmax(auto, 680px);
+    justify-content: center;
+    align-items: center;
   }
   .TodoBoom-nav {
     position: fixed;
     z-index: 1000;
     bottom: 0;
+    left: 0;
+    right: 0;
   }
 </style>
 
 <div class="TodoBoom">
+  <LogoStatic />
   <div class="TodoBoom-content">
     <slot />
   </div>
--- frontend/src/routes/workspace/index.svelte
+++ frontend/src/routes/workspace/index.svelte
@@ -1,7 +1,49 @@
+<script context="module">
+  import { fetchApi } from "../../utils";
+
+  export async function preload(page, session) {
+    const { token } = session;
+    if (!token) return this.redirect(302, "/login");
+
+    // Get user data
+    const { data: userResponse } = await fetchApi.get("/users/current", token);
+    const user = userResponse.data;
+    user.password = "";
+
+    // Get all todos
+    const { data: todosResponse } = await fetchApi.get("/todos/all", token);
+    const todosList = todosResponse.data;
+
+    return { userData: user, todosList };
+  }
+</script>
+
 <script>
+  export let userData;
+  export let todosList;
+
+  import { user, todos } from "../../context";
+  user.set(userData);
+  todos.set(todosList);
 </script>
 
 <style>
 </style>
 
-<div class="Login">Workspace</div>
+<div class="Workspace">
+  <div class="Workspace-container">
+    <div class="Worskpace-info">
+      <h2>Hello, <strong>{$user.name}</strong></h2>
+    </div>
+    <div class="Workspace-todos">
+      <div class="Todos-select">
+        <p>Today</p>
+      </div>
+      <div class="Todos-list">
+        {#each $todos as todo}
+          <div class="Todo-container">{todo.title}</div>
+        {:else}Not todos{/each}
+      </div>
+    </div>
+  </div>
+</div>
