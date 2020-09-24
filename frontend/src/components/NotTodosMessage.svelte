0674a73161ee801994f8f37b11300081e644846e
emanuelosva <emanuelosva@gmail.com>
2020-09-24 14:31:48 -0500

    Add not todos message components

---
+++ frontend/src/components/NotTodosMessage.svelte
@@ -0,0 +1,27 @@
+<script>
+</script>
+
+<style>
+  .NotTodosMessage-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  .NotTodosMessage img {
+    width: 260px;
+  }
+  .NotTodosMessage a {
+    font-size: 24px;
+    font-weight: 600;
+    text-decoration-color: #ff7f51;
+    cursor: pointer;
+  }
+</style>
+
+<div class="NotTodosMessage">
+  <div class="NotTodosMessage-container">
+    <img src="no-todos-image.png" alt="not todos" />
+    <a href="/workspace/new" rel="prefetch">Create your first Todo!</a>
+  </div>
+</div>
