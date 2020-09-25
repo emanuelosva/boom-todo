e95bc52dfed4c383bea65383313ba1d5a4843236
emanuelosva <emanuelosva@gmail.com>
2020-09-25 13:29:25 -0500

    Fix api path for production

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -4,13 +4,15 @@
  * **************************
  */
 
+const isDevMode = process.env.NODE_ENV !== 'production';
+
 export const config = {
   app: {
     port: process.env.PORT,
-    dev: process.env.NODE_ENV !== 'production',
+    dev: isDevMode,
     sessionTime: 2 * 24 * 3600 * 1000, // 2 Days
   },
   api: {
-    url: 'http://localhost:5000/v1',
+    url: isDevMode ? 'http://localhost:5000/v1' : '/v1',
   }
 };
